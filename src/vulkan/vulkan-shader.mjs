91f690dbc0d302fa368697e576e54f6c19710bd9
Paul Hirch <Paul-Jasper@hotmail.de>
2019-08-11 18:29:27 +0200

    Update vulkan-shader.mjs

--- src/vulkan/vulkan-shader.mjs
+++ src/vulkan/vulkan-shader.mjs
@@ -1,8 +1,11 @@
 import fs from "fs"
 import nvk from "nvk"
+import { pushHandle, deleteHandle } from "./utils.mjs";
 import { GLSL } from "nvk-essentials"
 
-Object.assign(global, nvk);
+export let shaderChanged = false;
+export let shaderHandles = [];
+export let shaderStages = new Array(10).fill(null);
 
 export function loadShaderSrc(path, type) {
   if (this.shaderSrcCache[path] === undefined)
@@ -12,6 +15,33 @@ export function loadShaderSrc(path, type) {
     }).output;
   return this.shaderSrcCache[path];
 }
+
+export function createShader(code) {
+  let shader = this.createShaderModule(code);
+
+  let handle = {
+    id: -1,
+    stage: -1,
+    shader: shader,
+  }
+
+  pushHandle(this.shaderHandles, handle);
+
+  return handle;
+}
+export function bindShader(handle, stage) {
+  if (this.shaderStages[stage] !== null) this.shaderStages[stage].stage = -1;
+  handle.stage = stage;
+  this.shaderStages[stage] = handle;
+}
+
+export function destroyShader(handle) {
+  if (handle.id === -1) return;
+  this.shaderChanged = true;
+  vkDestroyShaderModule(this.device, handle.shader, null);
+  deleteHandle(this.shaderHandles, handle);
+}
+
 
 export function createShaderModule(code) {
   let uIntCode = new Uint8Array(code);
